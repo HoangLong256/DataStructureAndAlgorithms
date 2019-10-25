066962a4a70ec0c24e8cf660a2424485d09e4eb2
Long Nguyen <longhnnguyen256@gmail.com>
2019-10-25 21:11:13 +0700

    Edit linear search

--- Sorting Algoritms/Search/Linear Search/linear.js
+++ Sorting Algoritms/Search/Linear Search/linear.js
@@ -1,7 +1,7 @@
 // Linear Search : Loop through the unsorted array to find searched elements
 // Complexity: O(n)
 
-lArray = [0,4,7,2,8,5,6];
+const lArray = [0,4,7,2,8,5,6];
 const linear = (lArray, number) =>{
     for(let i = 0; i < lArray.length; i ++){
         if( lArray[i] === number){
